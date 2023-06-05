8aa57e5f3b53abcbbbbb8cbe81a67deab2be730c
aniket <aniket.umbc@gmail.com>
2023-06-05 13:40:43 +0530

    Destrcture values

--- temlateString.js
+++ temlateString.js
@@ -16,11 +16,39 @@ function upper(static, ...dynamic) {
   console.log(str);
 }
 
-console.log(upper`Hello ${userName} (${twitter}), welcome to ${topic}`);
+// console.log(upper`Hello ${userName} (${twitter}), welcome to ${topic}`);
 
-const longString = `                            Hello ${userName} (${twitter}), welcome to ${topic}                               `;
+// const longString = `                            Hello ${userName} (${twitter}), welcome to ${topic}                               `;
 
-console.log(longString);
+// console.log(longString);
 
-console.log(longString.trimEnd());
-console.log(longString.trim());
+// console.log(longString.trimEnd());
+// console.log(longString.trim());
+
+/**
+ *
+ * Destructring
+ *
+ */
+
+/**
+ *  Imperative approch
+ */
+function data() {
+  return [1, 2, 3, 4, 5];
+}
+
+var temp = data();
+
+const firstOld = temp[0];
+const secondOld = temp[1];
+
+console.log('First', firstOld, 'and Second', secondOld);
+
+/**
+ *  Declarative approch
+ */
+
+const [first, second, third, forth, fifth, six] = data();
+
+console.log(first, second, third, forth, fifth, six);
